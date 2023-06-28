5f958dcf437c52c1c07b242c30c13a705848dfce
tamene gelaye <tamenes21@gmail.com>
2023-06-28 16:35:43 +0000

    commented

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,7 +1,10 @@
 export default function divideFunction(numerator, denominator) {
-  if (denominator !== 0) {
+  /* check if the denominator is zero */
+  if (denominator === 0) {
+    /* if so throw error message */
     throw Error('cannot divide by 0');
   } else {
+    /* otherwise return the result */
     return numerator / denominator;
   }
 }
