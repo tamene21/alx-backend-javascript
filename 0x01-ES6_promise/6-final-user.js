6e362229ccd6c8f379301a0af624bb45602ae8c3
tamene gelaye <tamenes21@gmail.com>
2023-06-28 15:36:55 +0000

    method camel case

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,7 +3,7 @@ import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
-    .allsettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((res) => (
       res.map((o) => ({
         status: o.status,
