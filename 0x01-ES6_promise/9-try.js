8db81fff0abd965333313c692833ab18e56b3507
tamene gelaye <tamenes21@gmail.com>
2023-06-28 17:02:56 +0000

    code cleaned

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,12 +1,17 @@
+/* a quardrail function accepting a function argument  */
 export default function guardrail(mathFunction) {
+  /* declaring empty array  */
   const queue = [];
-
+  /* try to compute the maths function and push to the array */
   try {
     queue.push(mathFunction());
   } catch (err) {
+    /* If the computation is not successful catch & push the error message to the array  */
     queue.push(String(err));
   } finally {
+    /* for every computation append the below message */
     queue.push('Guardrail was processed');
   }
+  /* return the array */
   return queue;
 }
