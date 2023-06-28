f17143a457a8a66fb9f9fa7f7c7f94852954d1b5
tamene gelaye <tamenes21@gmail.com>
2023-06-28 15:54:07 +0000

    commented

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,5 +1,7 @@
 export default function loadBalancer(chinaDownload, USDownload) {
   return Promise
+  /* Compare two promises */
     .race([chinaDownload, USDownload])
+  /* show the download result */
     .then((res) => res);
 }
