96cd0829dd6b84e569b37673ef3f47e94bd2728a
Tamene Gelaye <tamenes21@gmail.com>
2023-06-30 12:57:01 +0000

    object: computing value on object key

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -4,9 +4,10 @@ function getCurrentYear() {
 }
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
-  return {
-    [income]: `income-${getCurrentYear()}`,
-    [gdp]: `gdp-${getCurrentYear()}`,
-    [capita]: `capita-${getCurrentYear()}`,
+  const budget = {
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita,
   };
+  return budget;
 }
