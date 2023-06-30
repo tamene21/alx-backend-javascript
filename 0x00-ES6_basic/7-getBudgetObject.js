c45ba3482d44ea7143d26632ccf2046eba477812
Tamene Gelaye <tamenes21@gmail.com>
2023-06-30 12:34:25 +0000

    Object: shrtening its expression

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,6 +1,9 @@
 export default function getBudgetObject(income, gdp, capita) {
-  /* Using only keys */
-  const budget = { income, gdp, capita };
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
 
   return budget;
 }
