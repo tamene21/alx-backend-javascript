e7d99e5c430ee52da2b572736be66be10f933b83
Tamene Gelaye <tamenes21@gmail.com>
2023-06-30 19:51:10 +0000

    getter and setter

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,43 @@
+export default class HolbertonCourse {
+  Constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(value) {
+    if (value !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = value;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(value) {
+    if (!(value instanceof Array)) {
+      throw new TypeError('Student must be array of string ');
+    }
+    if (!value.every((student) => typeof student === 'string')) {
+      throw new TypeError('Student must be array of string');
+    }
+    this._student = value;
+  }
+}
