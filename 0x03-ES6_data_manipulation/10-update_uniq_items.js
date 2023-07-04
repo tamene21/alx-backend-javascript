4baa97ab819fa26c1d9560d78e3019151bd49530
Tamene Gelaye <tamenes21@gmail.com>
2023-07-04 20:54:38 +0000

    task 10

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,10 @@
+export default function updateUniqueItems(map) {
+  if (!(map instanceof map)) {
+    throw new Error('Cannot process');
+  }
+  map.forEach((value, key) => {
+    if (value === 1) {
+      map.set(key, 100);
+    }
+  });
+}
