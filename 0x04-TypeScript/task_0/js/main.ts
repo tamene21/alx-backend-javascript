e29d6d26f44c51c693d4f6b607b206dee02d15bc
Tamene Gelaye <tamenes21@gmail.com>
2023-07-05 13:49:49 +0000

    creating students list

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,27 @@
+export interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: strin;,
+ }
+/* Creating two studets using Student interface */
+const studentA:Student = {
+  firstName: 'Thomas';
+  lastName: 'chris';
+  age: 25;
+  location: German;
+  }
+
+const studentB: Student = {
+  firstName: Fantu;
+  lastName: Tada;
+  age: 23;
+  location: Ethiopia;
+  }
+
+/* creating an array for the above two students */
+
+const studentsList: Array<Student> = [
+  studentA;
+  studentB;  
+];
