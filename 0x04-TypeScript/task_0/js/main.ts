e47bb62812bcd1a0a9de8f44cf9f05cfae41a6f0
Tamene Gelaye <tamenes21@gmail.com>
2023-07-05 14:33:50 +0000

    task 0

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -23,4 +23,28 @@ const studentB: Student = {
 const studentsList: Array<Student> = [
   studentA;
   studentB;  
-];
+  ];
+
+  /* render a table and for each elements in the array using firstname and location */
+export const displayStudent = (students:Array<Students>)=> void {
+  const table = document.createElement('table');
+  const tableHead = document.createElement('thead');
+  const headRow = document.createElement('tr');
+  const tableBody = document.createElement('tbody');
+  headRow.insertAdjacentHTML('beforeend', <td>FirstName</td>);
+  headRow.insertAdjacentHTML('beforeend', <td>Location</td>);
+  tableHead.insertAdjacentElement('beforeend', headRow);
+
+  for (const student of students) {
+    const bodRow = document.createElement('tr');
+    bodyRow.insertAdjacentHTML('beforeend', `<td>${student.firstName}</td>`);
+    bodyRow.insertAdjacentHTML('beforeend', `<td>${student.location}</td>`);
+    tableBody.insertAdjacentElement('beforeend', bodyRow);
+    }
+
+    table.insertAdjacentElement('beforeend', tableHead);
+    table.insertAdjacentElement('beforeend', tablebody);
+    document.body.insertAdjacentElement('beforeend', table);
+}
+
+
