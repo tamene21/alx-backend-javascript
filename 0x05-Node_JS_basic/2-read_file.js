315839933917035de7b7725798e679e1d786549b
tamene gelaye <tamenes21@gmail.com>
2023-08-23 14:52:03 +0000

    mutltipe edit

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -4,32 +4,31 @@
 
 /* Using the filesystem module */
 const fs = require('fs');
-/* accessing the filepath */
+
 const countStudents = (path) => {
   if (!fs.existsSync(path)) {
     throw Error('Cannot load the database');
   }
+
   const data = fs.readFileSync(path, 'utf8');
   const students = data.split('\n')
     .map((student) => student.split(','))
     .filter((student) => student.length === 4 && student[0] !== 'firstname')
     .map((student) => ({
-      firstname: student[0],
-      lastname: student[1],
+      firstName: student[0],
+      lastName: student[1],
       age: student[2],
       field: student[3],
     }));
-
   const csStudents = students
     .filter((student) => student.field === 'CS')
-    .map((student) => student.firstname);
-
+    .map((student) => student.firstName);
   const sweStudents = students
     .filter((student) => student.field === 'SWE')
-    .map((student) => student.firstname);
+    .map((student) => student.firstName);
   console.log(`Number of students: ${students.length}`);
-  console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(',')}`);
-  console.log(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(',')}`);
+  console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}`);
+  console.log(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
 };
 
 module.exports = countStudents;
