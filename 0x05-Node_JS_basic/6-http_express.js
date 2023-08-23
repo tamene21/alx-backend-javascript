85e2808c8451499112d86b8ffac5c942f0ec2f44
tamene gelaye <tamenes21@gmail.com>
2023-08-23 16:50:25 +0000

    express app

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,16 @@
+/**
+ * server using express
+ */
+
+const express = require('express');
+
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
