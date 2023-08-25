318f5674350bba26e77ca25588c1b9f3a821cfb1
tamene gelaye <tamenes21@gmail.com>
2023-08-25 12:34:06 +0000

    variable definition

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -2,7 +2,7 @@ const Utils = require('./utils');
 
 
 const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
-  const toatlCost = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  const totalCost = Utils.calculateNumber('SUM', totalAmount, totalShipping);
   console.log(`The total is: ${totalCost}`);
 }
 
