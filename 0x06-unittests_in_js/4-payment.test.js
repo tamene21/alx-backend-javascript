809eb1b14f5189b9bc918fb679afd8907b05cc6d
tamene gelaye <tamenes21@gmail.com>
2023-08-25 13:07:08 +0000

    stubs

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,21 @@
+const sinon = require('sinon');
+const Utils = require('./utils');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./4-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  it('Use calculateNumber method for sending payment request', () =>{
+    const buyer = sinon.spy(Utils);
+    const spie = sinon.stub(Utils, 'calculateNumber');
+
+    spie.returns(10);
+
+    sendPaymentRequestToApi(100, 20);
+    expect(spie.calledWith('SUM', 100, 20)).to.be.true;
+    expect(spie.callCount).to.be.equal(1);
+    expect(buyer.log.calledWith('The total is: 10)).to.be.true;
+    expect(buyer.log.callCount).to.be.equal(1);
+    spie.restore();
+    buyer.log.restore();
+  });
+});
