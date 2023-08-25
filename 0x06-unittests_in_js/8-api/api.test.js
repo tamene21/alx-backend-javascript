05650f664d0870682c02e2acbbe8566f9fa89cbf
tamene gelaye <tamenes21@gmail.com>
2023-08-25 15:55:20 +0000

    body

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -8,7 +8,7 @@ describe('API integration test', () => {
   it('GET / returns correct response', (done) => {
     request.get(`${API_URL}/`, (_err, res, body) => {
       expect(res.statusCode).to.be.equal(200);
-      expect(body).to.equal('Welcome to the payment system');
+      expect(body).should.containEql('Welcome to the payment system');
       done();
     });
   });
