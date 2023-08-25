9ff98a1f574f3362e1ec5db40b560970cc8c6fd2
tamene gelaye <tamenes21@gmail.com>
2023-08-25 15:31:16 +0000

    URL

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -5,7 +5,7 @@ const { expect } = require('chai');
 describe('API integration test', () => {
   const API_URL = 'http://localhost:7865';
   it('GET/ returns correct response', (done) => {
-    request.get(`${API_URI}/`, (_err, res, body) => {
+    request.get(`${API_URL}/`, (_err, res, body) => {
       expect(res.statusCode).to.be.equal(200);
       expect(body).to.be.equal('Welcome to the payment system');
       done();
