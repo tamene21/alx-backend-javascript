136b9301267d2a180a2f8acaac1255cca4fc5338
tamene gelaye <tamenes21@gmail.com>
2023-08-25 16:09:39 +0000

    body messafe

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,20 +1,14 @@
-const { expect } = require("chai");
 const request = require('request');
+const { expect } = require('chai');
 
+describe('API integration test', () => {
+  const API_URL = 'http://localhost:7865';
 
-describe('Integration Testing', () => {
-    describe('GET /', () => {
-      it('Code: 200 | Body: Welcome to the payment system', (done) => {
-        const options = {
-          url: 'http://localhost:7865',
-          method: 'GET',
-        };
-
-        request(options, function (error, response, body) {
-          expect(response.statusCode).to.equal(200);
-          expect(body).to.equal('Welcome to the payment system');
-          done();
-        });
-      });
-   });
+  it('GET / returns correct response', (done) => {
+    request.get(`${API_URL}/`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
+      done();
+    });
+  });
 });
