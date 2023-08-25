f022c3928678a7fc85f53ae81f4d71b9a1e31d5e
tamene gelaye <tamenes21@gmail.com>
2023-08-25 14:42:25 +0000

    use to.include

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -5,7 +5,7 @@ describe('getPaymentTokenFromAPI', () => {
   it('getPaymentTokenFromAPI(success), where success == true', (done) => {
     getPaymentTokenFromAPI(true)
       .then((res) => {
-        expect(res).to.deep.equal({data: 'Successful response from the API'});
+        expect(res).to.include({data: 'Successful response from the API'});
         done();
       })
       .catch((err) => done(err));
