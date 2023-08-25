fe79afbc4703ee866502b5256f1e6ad6640fd68d
tamene gelaye <tamenes21@gmail.com>
2023-08-25 12:28:55 +0000

    payment test

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,15 @@
+const sinon = require('sinon');
+const Utils = require('./utils');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./3-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  it('Use calculateNumber method for sending payment request', () =>{
+    const buyer = sinon.spy(Utils);
+
+    sendPaymentRequestToApi(100, 20);
+    expect(buyer.calculateNumber.calledwith('SUM', 100, 20)).to.be.true;
+    expect(buyer.calculateNumber.callCount).to.be.equal(1);
+    buyer.calculateNumber.restore();
+  });
+});
